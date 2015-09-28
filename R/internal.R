b93eea456966f7c041fdb667c9a3f496b1c16da7
Andreas Eckner <andreas@eckner.com>
2015-09-28 11:28:31 -0400

    Added help page with list of internal functions

---
+++ R/internal.R
@@ -0,0 +1,25 @@
+#' Internal Functions
+#'
+#' The internal functions listed below might be of interest to developers seeking to extend the package.
+#' 
+#' C interfaces:
+#' \itemize{
+#'   \item \code{\link{generic_C_interface}}
+#'   \item \code{\link{generic_C_interface_rolling}}
+#' }
+#' 
+#' \code{uts} methods:
+#' \itemize{
+#'   %\item \code{\link{ema_equal_R}}
+#'   %\item \code{\link{ema_last_R}}
+#'   %\item \code{\link{ema_next_R}}
+#'   %\item \code{\link{ema_linear_R}
+#'   \item \code{\link{sma_equal_R}}
+#'   %\item \code{\link{sma_last_R}}
+#'   %\item \code{\link{sma_next_R}}
+#'   %\item \code{\link{sma_linear_R}}
+#'   \item \code{\link{rev}}
+#' }
+#' 
+#' @name utsOperators-internal
+NULL
