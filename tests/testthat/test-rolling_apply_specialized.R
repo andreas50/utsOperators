c41d5005592530a7a9cce63a07ca29b1254714f0
Andreas Eckner <andreas@eckner.com>
2015-11-06 16:02:57 -0500

    Added more rolling_apply_specialized() tests

--- tests/testthat/test-rolling_apply_specialized.R
+++ tests/testthat/test-rolling_apply_specialized.R
@@ -14,10 +14,29 @@ test_that("rolling_apply_specialized argument checking",{
 
 
 test_that("rolling_apply_specialized gives the same results as rolling_apply",{
+  # FUN = length
+  expect_equal(
+    rolling_apply_specialized(ex_uts(), ddays(1), FUN=length),
+    rolling_apply(ex_uts(), width=ddays(1), FUN=length)
+  )
+  
+  # FUN = min
+  expect_equal(
+    rolling_apply_specialized(ex_uts(), ddays(1), FUN=min),
+    rolling_apply(ex_uts(), width=ddays(1), FUN=min)
+  )
+  
+  # FUN = max
+  expect_equal(
+    rolling_apply_specialized(ex_uts(), ddays(1), FUN=max),
+    rolling_apply(ex_uts(), width=ddays(1), FUN=max)
+  )
+  
   # FUN = sum
   expect_equal(
     rolling_apply_specialized(ex_uts(), ddays(1), FUN=sum),
     rolling_apply(ex_uts(), width=ddays(1), FUN=sum)
   )
 })
+
 
