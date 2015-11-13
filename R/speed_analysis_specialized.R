0f20521c824120989d873652f3be78623a2e013c
Andreas Eckner <andreas@eckner.com>
2015-11-13 14:06:25 -0500

    more R vs. C speed analysis

--- R/speed_analysis_specialized.R
+++ R/speed_analysis_specialized.R
@@ -18,7 +18,7 @@ if (0) {
 }
 
 
-### rolling_apply_specialized vs. rolling_apply
+### rolling_apply_specialized vs. rolling_apply for FUN=sum
 # -) the specialized implementation is ~70 times faster
 if (0) {
   set.seed(1)
@@ -37,3 +37,44 @@ if (0) {
   Rprof(NULL)
   summaryRprof()
 }
+
+
+### Same, but for FUN=min/max
+# -) the specialized implementation is ~70 times faster
+if (0) {
+  set.seed(1)
+  ts1 <- uts(rnorm(1000), as.POSIXct("2000-01-01") + ddays(1:1000))
+  width <- ddays(100)
+  
+  # generic vs. specialized: 1.34s vs. 1.90s
+  system.time(for (j in 1:200) rolling_apply(ts1, width, FUN=min))
+  system.time(for (j in 1:20000) rolling_apply_specialized(ts1, width, FUN=min))
+  
+  # Profile specialized implementation
+  # -) less than 15% of time spent in C code
+  Rprof(interval=0.01)
+  for (j in 1:20000) rolling_apply_specialized(ts1, width, FUN=min)
+  Rprof(NULL)
+  summaryRprof()
+}
+
+
+### Same, but for FUN=median
+# -) the specialized implementation is ~35 times faster
+if (0) {
+  set.seed(1)
+  ts1 <- uts(rnorm(1000), as.POSIXct("2000-01-01") + ddays(1:1000))
+  width <- ddays(100)
+  
+  # generic vs. specialized: 3.99s vs. 1.14s
+  system.time(for (j in 1:100) rolling_apply(ts1, width, FUN=median))
+  system.time(for (j in 1:1000) rolling_apply_specialized(ts1, width, FUN=median))
+  
+  # Profile specialized implementation
+  # -) ~90% of time spent in C code
+  Rprof(interval=0.01)
+  for (j in 1:2000) rolling_apply_specialized(ts1, width, FUN=median)
+  Rprof(NULL)
+  summaryRprof()
+}
+
