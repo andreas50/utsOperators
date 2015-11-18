438c66861db2e82ed48eb66543737775cd158439
Andreas Eckner <andreas@eckner.com>
2015-11-18 13:53:50 -0500

    Added one- vs. two-side rolling_num_obs speed analysis

--- R/speed_analysis_specialized.R
+++ R/speed_analysis_specialized.R
@@ -67,3 +67,21 @@ if (0) {
   summaryRprof()
 }
 
+
+### rolling_num_obs vs. rolling_num_obs_two_sided
+if (0) {
+  set.seed(1)
+  ts1 <- uts(rnorm(1000), as.POSIXct("2000-01-01") + ddays(1:1000))
+  width <- ddays(100)
+  width2 <- ddays(0)
+  
+  # one sided vs. two-sided implementation with same  window
+  # -) 2.19s vs. 2.29s -> negligible difference
+  system.time(for (j in 1:50000) generic_C_interface(ts1, "rolling_num_obs", width=width))
+  system.time(for (j in 1:50000) generic_C_interface(ts1, "rolling_num_obs_two_sided", width_before=width, width_after=width2))
+}
+
+
+
+
+
