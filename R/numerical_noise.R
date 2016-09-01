2af32d73eb3fe3f95d458dbef7089d09c2be1f28
Andreas Eckner <andreas@eckner.com>
2016-09-01 10:09:52 -0400

    More numerical noise analysis

--- R/numerical_noise.R
+++ R/numerical_noise.R
@@ -2,35 +2,52 @@
 # Examine numerical noise across implementations #
 ##################################################
 
-# Very long long time series
-# -) the R implementations is free of numerical noise, because the value for each window is calculated from scratch
+# Really long rolling time window
 if (0) {
-  x <- ex_uts3(10000) ^ 4
-  #x <- uts(values=rep(c(1e6, 1.2, 1.3, 1.4, 1e-10), length=1e4),  times=as.POSIXct("2016-01-01") + days(1:1e4))
-  width <- ddays(10)
+  x <- abs(ex_uts3(1e5))
+  width <- ddays(1e4)
   
   # rolling_sum in R vs. roll_sum in C
-  # -) the R & C implementation diverge over time.
-  # -) the problem becomes more serious for longer, heavily skewed time series
-  tmp <- rolling_apply_specialized(x, width, FUN=sum) / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
-  plot(tmp)
-  sd(tmp)
+  # -) the R & C implementation diverge over time
+  rel_err <- rolling_apply_specialized(x, width, FUN=sum) / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
+  plot(rel_err)
+  sd(rel_err)
   
   # rolling_sum in R vs. roll_sum_stable in C
-  # -) standard deviation of differences ~65% lower
-  tmp <- rolling_apply_specialized(x, width, FUN="sum_stable") / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
-  plot(tmp)
-  sd(tmp)
-  
-  # sum() and kahansum() give the same result
-  library(cmna)
-  tmp <- rolling_apply(x, width, FUN=kahansum) / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
-  plot(tmp)
-  sd(tmp)
+  # -) standard deviation of numeric error ~99% lower, i.e. essentially disappears
+  rel_err <- rolling_apply_specialized(x, width, FUN="sum_stable") / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
+  plot(rel_err)
+  sd(rel_err)
 }
 
 
-# Time series with extremely skewed observation values
+# Heavily skewd observation values
+# -) the R implementations is free of numerical noise, because the value for each window is calculated from scratch
+if (0) {
+  x <- ex_uts3(5000) ^ 10
+  width <- ddays(10)
+  
+  # rolling_sum in R vs. roll_sum in C
+  # -) the R & C implementation diverge over time
+  rel_err <- rolling_apply_specialized(x, width, FUN=sum) / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
+  plot(rel_err)
+  sd(rel_err)
+  
+  # rolling_sum in R vs. roll_sum_stable in C
+  # -) standard deviation of numeric error ~85% lower
+  rel_err <- rolling_apply_specialized(x, width, FUN="sum_stable") / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
+  plot(rel_err)
+  sd(rel_err)
+  
+  # sum() and kahansum() give the same result
+  library(cmna)
+  rel_err <- rolling_apply(x, width, FUN=kahansum) / rolling_apply(x, width, FUN=sum, use_specialized=FALSE) - 1
+  plot(rel_err)
+  sd(rel_err)
+}
+
+
+# Extremely skewed observation values
 if (0) {
   x <- uts(values=rep(c(1e16, 1, -1e16), 10), times=as.POSIXct("2016-01-01") + days(1:30))
   width <- ddays(0.5)
